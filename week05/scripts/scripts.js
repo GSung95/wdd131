0ed91201b5be00cabfc21301ad4693819e090b4a
GSung95 <gene.sunglao@gmail.com>
2024-06-06 19:03:43 +0800

    week 5 assignment comit

--- week05/scripts/scripts.js
+++ week05/scripts/scripts.js
@@ -15,3 +15,13 @@ window.addEventListener('DOMContentLoaded', (event) => {
         productSelect.appendChild(option);
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get current year
+    var currentYear = new Date().getFullYear();
+    document.getElementById("currentyear").textContent = currentYear;
+
+    // Get last modified date
+    var lastModifiedDate = document.lastModified;
+    document.getElementById("lastModified").textContent = "Last modified: " + lastModifiedDate;
+});
