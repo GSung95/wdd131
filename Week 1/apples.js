2c3595578e80c014c4ddaf5737d2e7de06e0447f
GSung95 <gene.sunglao@gmail.com>
2025-01-24 19:59:25 +0800

    week 1 assignment

---
+++ Week 1/apples.js
@@ -0,0 +1,7 @@
+const myApples = 4;
+const friendApples = 2;
+let total = myApples + friendApples;
+
+document.getElementById("myAppleElement").textContent = myApples;
+document.getElementById("friendAppleElement").textContent = friendApples;
+document.getElementById("totalApplesElement").textContent = total;
---
+++ Week 1/running.js
@@ -0,0 +1,4 @@
+const apples = 5;
+const oranges = 3;
+let total = apples + oranges;
+console.log("total:", total);
